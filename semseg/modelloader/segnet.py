c507420fc1b91ebc594ec0168143dd960ac100f7
guanfuchen <guanfuchen@zju.edu.cn>
2017-12-28 10:55:50 +0800

    增加segnet初始化vgg

--- semseg/modelloader/segnet.py
+++ semseg/modelloader/segnet.py
@@ -10,7 +10,7 @@ from torch.autograd import Variable
 from torchvision import models
 
 from semseg.loss import cross_entropy2d
-from semseg.modelloader.utils import segnetDown2, segnetDown3, segnetUp2, segnetUp3
+from semseg.modelloader.utils import segnetDown2, segnetDown3, segnetUp2, segnetUp3, conv2DBatchNormRelu
 
 
 class segnet(nn.Module):
@@ -28,8 +28,7 @@ class segnet(nn.Module):
         self.up2 = segnetUp2(128, 64)
         self.up1 = segnetUp2(64, n_classes)
 
-        # if pretrained:
-        #     self.init_vgg16()
+        self.init_vgg16(pretrained=pretrained)
 
     def forward(self, x):
         x, pool_indices1, unpool_shape1 = self.down1(x)
@@ -45,28 +44,52 @@ class segnet(nn.Module):
         x = self.up1(x, pool_indices=pool_indices1, unpool_shape=unpool_shape1)
         return x
 
-    def init_vgg16(self):
-        vgg16 = models.vgg16(pretrained=True)
+    def init_vgg16(self, pretrained=False):
+        vgg16 = models.vgg16(pretrained=pretrained)
 
         # -----------赋值前面2+2+3+3+3层feature的特征-------------
         # 由于vgg16的特征是Sequential，获得其中的子类通过children()
         vgg16_features = list(vgg16.features.children())
+        vgg16_conv_layers = []
+        for layer in vgg16_features:
+            if isinstance(layer, nn.Conv2d):
+                # print(layer)
+                vgg16_conv_layers.append(layer)
+
 
         conv_blocks = [self.down1, self.down2, self.down3, self.down4, self.down5]
         conv_ids_vgg = [[0, 4], [5, 9], [10, 16], [17, 23], [24, 30]]
 
+        segnet_down_conv_layers = []
         for conv_block_id, conv_block in enumerate(conv_blocks):
             # print(conv_block_id)
-            conv_id_vgg = conv_ids_vgg[conv_block_id]
-            for l1, l2 in zip(conv_block, vgg16_features[conv_id_vgg[0]:conv_id_vgg[1]]):
-                if isinstance(l1, nn.Conv2d) and isinstance(l2, nn.Conv2d):
-                    assert l1.weight.size() == l2.weight.size()
-                    assert l1.bias.size() == l2.bias.size()
-                    # 赋值的是数据
-                    l1.weight.data = l2.weight.data
-                    l1.bias.data = l2.bias.data
-                    # print(l1)
-                    # print(l2)
+            # print(conv_block)
+            conv_block_children =  list(conv_block.children())
+            for conv_block_child in conv_block_children:
+                if isinstance(conv_block_child, conv2DBatchNormRelu):
+                    # print(conv_block_child)
+                    if hasattr(conv_block_child, 'cbr_seq'):
+                        # print(conv_block_child.cbr_seq)
+                        layer_lists = list(conv_block_child.cbr_seq)
+                        for layer in conv_block_child.cbr_seq:
+                            # print(layer)
+                            if isinstance(layer, nn.Conv2d):
+                                # print(layer)
+                                segnet_down_conv_layers.append(layer)
+
+        # print('len(segnet_down_conv_layers):', len(segnet_down_conv_layers))
+        # print('len(vgg16_conv_layers)', len(vgg16_conv_layers))
+
+        for l1, l2 in zip(segnet_down_conv_layers, vgg16_conv_layers):
+            if isinstance(l1, nn.Conv2d) and isinstance(l2, nn.Conv2d):
+                assert l1.weight.size() == l2.weight.size()
+                assert l1.bias.size() == l2.bias.size()
+                # 赋值的是数据
+                l1.weight.data = l2.weight.data
+                l1.bias.data = l2.bias.data
+                # print(l1)
+                # print(l2)
+
 
 if __name__ == '__main__':
     n_classes = 21
