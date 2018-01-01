4883dcbfaf6de331c5c6ac7af184d0907ef6e6d9
guanfuchen <guanfuchen@zju.edu.cn>
2018-01-02 00:10:14 +0800

    从上层仓库中增加linknet，但是该模型依旧有问题

--- semseg/modelloader/utils.py
+++ semseg/modelloader/utils.py
@@ -9,6 +9,19 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 from torchvision import models
 
+class conv2DBatchNorm(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size,  stride, padding, bias=True):
+        super(conv2DBatchNorm, self).__init__()
+
+        self.cb_seq = nn.Sequential(
+            nn.Conv2d(int(in_channels), int(out_channels), kernel_size=kernel_size, padding=padding, stride=stride, bias=bias),
+            nn.BatchNorm2d(int(out_channels)),
+        )
+
+    def forward(self, inputs):
+        outputs = self.cb_seq(inputs)
+        return outputs
+
 class conv2DBatchNormRelu(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride, padding, bias=True):
         super(conv2DBatchNormRelu, self).__init__()
@@ -89,3 +102,60 @@ class segnetUp3(nn.Module):
         x = self.conv2(x)
         x = self.conv3(x)
         return x
+
+class residualBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
+        super(residualBlock, self).__init__()
+
+        self.convbnrelu1 = conv2DBatchNormRelu(in_channels, out_channels, 3,  stride, 1, bias=False)
+        self.convbn2 = conv2DBatchNorm(out_channels, out_channels, 3, 1, 1, bias=False)
+        self.downsample = downsample
+        self.stride = stride
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        residual = x
+
+        out = self.convbnrelu1(x)
+        out = self.convbn2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+        return out
+
+class linknetUp(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(linknetUp, self).__init__()
+
+        # B, 2C, H, W -> B, C/2, H, W
+        self.convbnrelu1 = conv2DBatchNormRelu(in_channels, out_channels/2, kernel_size=1, stride=1, padding=1)
+
+        # B, C/2, H, W -> B, C/2, H, W
+        self.deconvbnrelu2 = deconv2DBatchNormRelu(out_channels/2, out_channels/2, kernel_size=3,  stride=2, padding=0,)
+
+        # B, C/2, H, W -> B, C, H, W
+        self.convbnrelu3 = conv2DBatchNormRelu(out_channels/2, out_channels, kernel_size=1, stride=1, padding=1)
+
+    def forward(self, x):
+        x = self.convbnrelu1(x)
+        x = self.deconvbnrelu2(x)
+        x = self.convbnrelu3(x)
+        return x
+
+class deconv2DBatchNormRelu(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, bias=True):
+        super(deconv2DBatchNormRelu, self).__init__()
+
+        self.dcbr_unit = nn.Sequential(nn.ConvTranspose2d(int(in_channels), int(out_channels), kernel_size=kernel_size,
+                                                padding=padding, stride=stride, bias=bias),
+                                 nn.BatchNorm2d(int(out_channels)),
+                                 nn.ReLU(inplace=True),)
+
+    def forward(self, inputs):
+        outputs = self.dcbr_unit(inputs)
+        return outputs
