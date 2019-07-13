522543d04e156f6c1121723ffea0db45d67d458d
guanfuchen <guanfuchen@zju.edu.cn>
2019-07-13 22:21:57 +0800

    增加transform

---
+++ misc/transform.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+import random
+import collections
+import logging as log
+import torch
+import numpy as np
+from PIL import Image, ImageOps
+import time
+import cv2
+import matplotlib.pyplot as plt
+
+
+def randomCropLetterboxPil(img):
+    output_w, output_h = (1408, 768)
+    jitter = 0.3
+    fill_color = 127
+
+    orig_w, orig_h = img.size
+    img_np = np.array(img)
+    channels = img_np.shape[2] if len(img_np.shape) > 2 else 1
+    dw = int(jitter * orig_w)
+    dh = int(jitter * orig_h)
+    new_ar = float(orig_w + random.randint(-dw, dw)) / (orig_h + random.randint(-dh, dh))
+    scale = random.random() * (2 - 0.25) + 0.25
+    if new_ar < 1:
+        nh = int(scale * orig_h)
+        nw = int(nh * new_ar)
+    else:
+        nw = int(scale * orig_w)
+        nh = int(nw / new_ar)
+
+    if output_w > nw:
+        dx = random.randint(0, output_w - nw)
+    else:
+        dx = random.randint(output_w - nw, 0)
+
+    if output_h > nh:
+        dy = random.randint(0, output_h - nh)
+    else:
+        dy = random.randint(output_h - nh, 0)
+
+    nxmin = max(0, -dx)
+    nymin = max(0, -dy)
+    nxmax = min(nw, -dx + output_w - 1)
+    nymax = min(nh, -dy + output_h - 1)
+    sx, sy = float(orig_w) / nw, float(orig_h) / nh
+    orig_xmin = int(nxmin * sx)
+    orig_ymin = int(nymin * sy)
+    orig_xmax = int(nxmax * sx)
+    orig_ymax = int(nymax * sy)
+    orig_crop = img.crop((orig_xmin, orig_ymin, orig_xmax, orig_ymax))
+    orig_crop_resize = orig_crop.resize((nxmax - nxmin, nymax - nymin))
+    output_img = Image.new(img.mode, (output_w, output_h), color=(fill_color,) * channels)
+    output_img.paste(orig_crop_resize, (0, 0))
+    return output_img
+
+
+def randomCropLetterboxCv(img):
+    output_w, output_h = (1408, 768)
+    jitter = 0.3
+    fill_color = 127
+
+    # orig_w, orig_h = img.size
+    orig_h, orig_w = img.shape[:2]
+    channels = img.shape[2] if len(img.shape) > 2 else 1
+    dw = int(jitter * orig_w)
+    dh = int(jitter * orig_h)
+    new_ar = float(orig_w + random.randint(-dw, dw)) / (orig_h + random.randint(-dh, dh))
+    scale = random.random() * (2 - 0.25) + 0.25
+    if new_ar < 1:
+        nh = int(scale * orig_h)
+        nw = int(nh * new_ar)
+    else:
+        nw = int(scale * orig_w)
+        nh = int(nw / new_ar)
+
+    if output_w > nw:
+        dx = random.randint(0, output_w - nw)
+    else:
+        dx = random.randint(output_w - nw, 0)
+
+    if output_h > nh:
+        dy = random.randint(0, output_h - nh)
+    else:
+        dy = random.randint(output_h - nh, 0)
+
+    nxmin = max(0, -dx)
+    nymin = max(0, -dy)
+    nxmax = min(nw, -dx + output_w - 1)
+    nymax = min(nh, -dy + output_h - 1)
+    sx, sy = float(orig_w) / nw, float(orig_h) / nh
+    orig_xmin = int(nxmin * sx)
+    orig_ymin = int(nymin * sy)
+    orig_xmax = int(nxmax * sx)
+    orig_ymax = int(nymax * sy)
+    # orig_crop = img.crop((orig_xmin, orig_ymin, orig_xmax, orig_ymax))
+    orig_crop = img[orig_ymin:orig_ymax, orig_xmin:orig_xmax, :]
+    orig_crop_resize = cv2.resize(orig_crop, (nxmax - nxmin, nymax - nymin), interpolation=cv2.INTER_CUBIC)
+    output_img = np.ones((output_h, output_w, channels), dtype=np.uint8) * fill_color
+
+    y_lim = int(min(output_img.shape[0], orig_crop_resize.shape[0]))
+    x_lim = int(min(output_img.shape[1], orig_crop_resize.shape[1]))
+    output_img[:y_lim, :x_lim, :] = orig_crop_resize[:y_lim, :x_lim, :]
+    # orig_crop_resize = orig_crop.resize((nxmax - nxmin, nymax - nymin))
+    # output_img = Image.new(img.mode, (output_w, output_h), color=(fill_color,) * channels)
+    # output_img.paste(orig_crop_resize, (0, 0))
+    return output_img
+
+
+if __name__ == '__main__':
+    # test_num = 1
+    # start_time = time.time()
+    # for _ in range(test_num):
+    #     img = Image.open('../data/0006R0_f00930.png')
+    #     out_img = randomCropLetterboxPil(img)
+    #     plt.imshow(out_img)
+    #     plt.show()
+    # end_time = time.time()
+    # print('cost time:', (end_time-start_time)/test_num)
+
+    test_num = 100
+    img = cv2.imread('../data/0006R0_f00930.png')
+    img = cv2.resize(img, (1024, 720), interpolation=cv2.INTER_CUBIC)
+    print('img.shape:', img.shape)
+    start_time = time.time()
+    for _ in range(test_num):
+        out_img = randomCropLetterboxCv(img)
+        # cv2.imshow('img', out_img)
+        # cv2.waitKey(0)
+    end_time = time.time()
+    print('cost time:', (end_time-start_time)/test_num)
--- semseg/dataloader/tfrecords_loader.py
+++ semseg/dataloader/tfrecords_loader.py
@@ -32,6 +32,67 @@ import random
 import matplotlib.pyplot as plt
 
 
+def _dtype_feature(ndarray):
+    """match appropriate tf.train.Feature class with dtype of ndarray. """
+    assert isinstance(ndarray, np.ndarray)
+    dtype_ = ndarray.dtype
+    if dtype_ == np.float64 or dtype_ == np.float32:
+        return lambda array: tf.train.Feature(float_list=tf.train.FloatList(value=array))
+    elif dtype_ == np.int64:
+        return lambda array: tf.train.Feature(int64_list=tf.train.Int64List(value=array))
+    else:
+        raise ValueError("The input should be numpy ndarray. \
+                           Instaed got {}".format(ndarray.dtype))
+
+
+def write_tfrecords_batch(start, end, labels, flower_dir, file):
+    writer = tf.python_io.TFRecordWriter(file)
+    batch = 2
+    widths = [236, 256, 276]
+    heights = [236, 256, 276]
+
+    width_resize = widths[0]
+    height_resize = heights[0]
+    imgs = []
+    for id in range(start, end):
+        img = Image.open(flower_dir[id])
+        # label = labels[id]
+        label = 0
+        width, height = img.size
+        # h = 500
+        # x = int((width - h) / 2)
+        # y = int((height - h) / 2)
+        # img_crop = img.crop([x, y, x + h, y + h])
+        # img_500 = img_crop.tobytes()
+        img = img.resize((width_resize, height_resize))
+        imgs.append(np.array(img).transpose(2, 1, 0))
+        if (id+1)%batch==0 and id!=0:
+            imgs = np.array(imgs, dtype=np.float)
+            imgs = imgs.reshape(-1)
+            print('imgs.shape:', imgs.shape)
+            pass
+            example = tf.train.Example(features=tf.train.Features(feature={
+                # 'label': tf.train.Feature(int64_list=tf.train.Int64List(value=[label])),
+                # 'img': tf.train.Feature(bytes_list=tf.train.BytesList(value=[img_500])),
+                # 'img_batch': tf.train.Feature(bytes_list=tf.train.BytesList(value=[img.tobytes()])),
+                # 'img_batch': _dtype_feature(imgs),
+                # tf.train.Features(feature={"bytes": _floats_feature(numpy_arr)})
+                # 'height': tf.train.Feature(int64_list=tf.train.Int64List(value=[500])),
+                # 'width': tf.train.Feature(int64_list=tf.train.Int64List(value=[500]))
+                'imgs': tf.train.Feature(float_list=tf.train.FloatList(value=imgs)),
+                'height': tf.train.Feature(int64_list=tf.train.Int64List(value=[height_resize])),
+                'width': tf.train.Feature(int64_list=tf.train.Int64List(value=[width_resize]))
+            }))
+            # writer_500.write(example_500.SerializeToString())
+            # print(example)
+            writer.write(example.SerializeToString())
+            imgs = []
+            resize_id = random.randint(0, 2)
+            width_resize = widths[resize_id]
+            height_resize = heights[resize_id]
+
+
+
 def write_tfrecords(start, end, labels, flower_dir, file):
     writer = tf.python_io.TFRecordWriter(file)
     for id in range(start, end):
@@ -57,7 +118,7 @@ def write_tfrecords(start, end, labels, flower_dir, file):
         writer.write(example.SerializeToString())
 
 
-def flower_preprocess(flower_folder='/Users/cgf/Data/tmp/tfrecords'):
+def flower_preprocess(flower_folder='/Users/cgf/Data/tmp/tfrecords', tf_fn="flower_train.tfrecords"):
 
     labels = [0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2, 0,
               1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2]
@@ -70,8 +131,9 @@ def flower_preprocess(flower_folder='/Users/cgf/Data/tmp/tfrecords'):
     flower_dir.sort()
 
     length = int(len(flower_dir)*0.7)
-    file = "flower_train.tfrecords"
-    write_tfrecords(0, length, labels, flower_dir, file)
+    # file = "flower_train.tfrecords"
+    # write_tfrecords(0, length, labels, flower_dir, tf_fn)
+    write_tfrecords_batch(0, length, labels, flower_dir, tf_fn)
 
 
 class TFRecordsProxy():
@@ -80,33 +142,47 @@ class TFRecordsProxy():
         self.data_size = data_size
 
     def parse_data(self, example_proto):
-        features = {'img': tf.FixedLenFeature([], tf.string, ''),
-                    'label': tf.FixedLenFeature([], tf.int64, 0),
-                    'width': tf.FixedLenFeature([], tf.int64, 0),
-                    'height': tf.FixedLenFeature([], tf.int64, 0),
+        features = {
+                    # 'img': tf.FixedLenFeature([], tf.string, ''),
+                    'imgs': tf.VarLenFeature(tf.float32),
+                    # 'label': tf.FixedLenFeature([], tf.int64, 0),
+                    # 'width': tf.FixedLenFeature([], tf.int64, 0),
+                    # 'height': tf.FixedLenFeature([], tf.int64, 0),
                     }
         parsed_features = tf.parse_single_example(example_proto, features)
-        image = tf.decode_raw(parsed_features['img'], tf.uint8)
-        label = tf.cast(parsed_features['label'], tf.int64)
-        width = tf.cast(parsed_features['width'], tf.int64)
-        height = tf.cast(parsed_features['height'], tf.int64)
-        image = tf.reshape(image, tf.stack([height, width, 3]))
+        # imgs = tf.decode_raw(parsed_features['imgs'], tf.uint8)
+        label = 0
+        imgs = tf.cast(parsed_features['imgs'], tf.float32)
+        # width = tf.cast(parsed_features['width'], tf.int64)
+        # height = tf.cast(parsed_features['height'], tf.int64)
+        # image = tf.reshape(image, tf.stack([height, width, 3]))
         # print('width:', width)
         # print('height:', height)
         # image = tf.reshape(image, [height, width, 3])
         # print('image:', image)
-        return image, label
+        return imgs, label
         # return width, label
 
     def my_input_fn(self, filenames, data_size):
-        dataset = tf.contrib.data.TFRecordDataset(filenames)
-        dataset = dataset.map(self.parse_data)
-        # dataset = dataset.batch(data_size)
+        reader = tf.TFRecordReader()
 
-        # dataset = dataset.repeat()
-        iterator = dataset.make_one_shot_iterator()
-        features, labels = iterator.get_next()
-        return features, labels
+        filename_queue = tf.train.string_input_producer([filenames], num_epochs=1)
+        _, serialized_example = reader.read(filename_queue)
+        batch = tf.train.batch(tensors=[serialized_example], batch_size=1)
+        features = {
+                    'imgs': tf.VarLenFeature(tf.float32),
+                    }
+        key_parsed = tf.parse_example(batch, features)
+        # print tf.contrib.learn.run_n(key_parsed)
+        # dataset = tf.contrib.data.TFRecordDataset(filenames)
+        # dataset = dataset.map(self.parse_data)
+        # # dataset = dataset.batch(data_size)
+        #
+        # # dataset = dataset.repeat()
+        # iterator = dataset.make_one_shot_iterator()
+        # features, labels = iterator.get_next()
+        # return features, labels
+        return key_parsed, 1
 
     def main(self):
         features, labels = self.my_input_fn(self.filenames, self.data_size)
@@ -141,10 +217,11 @@ class TFRecordsDataset(torch.utils.data.Dataset):
 if __name__ == "__main__":
     pass
 
-    # flower_preprocess()
+    # flower_preprocess(tf_fn="flower_train_batch.tfrecords")
 
-    # tf_fn = os.path.join(os.path.expanduser('~/GitHub/Quick/pytorch-tfrecords/flower_train.tfrecords'))
-    tf_fn = os.path.join(os.path.expanduser('flower_train.tfrecords'))
+    tf_fn = os.path.join(os.path.expanduser('~/GitHub/Quick/pytorch-tfrecords/flower_train.tfrecords'))
+    # tf_fn = os.path.join(os.path.expanduser('flower_train.tfrecords'))
+    tf_fn = os.path.join(os.path.expanduser('flower_train_batch.tfrecords'))
     data_sz = 10
     dataset = TFRecordsDataset(tf_fn, data_sz)
     data_loader = torch.utils.data.DataLoader(dataset=dataset, batch_size=5, shuffle=True)
