eb58b5bd7df36a364330f6af113f0e68a20378cb
guanfuchen <guanfuchen@zju.edu.cn>
2019-07-13 23:38:27 +0800

    update

--- misc/transform.py
+++ misc/transform.py
@@ -107,25 +107,111 @@ def randomCropLetterboxCv(img):
     return output_img
 
 
-if __name__ == '__main__':
-    # test_num = 10
-    # img = Image.open('../data/0006R0_f00930.png')
-    # start_time = time.time()
-    # for _ in range(test_num):
-    #     out_img = randomCropLetterboxPil(img)
-    #     plt.imshow(out_img)
-    #     plt.show()
-    # end_time = time.time()
-    # print('cost time:', (end_time-start_time)/test_num)
+def randomFlipPil(img):
+    """ Randomly flip image """
+    flip = random.random() < 0.5
+    if flip:
+        img = img.transpose(Image.FLIP_LEFT_RIGHT)
+    return img
 
-    test_num = 1000
-    img = cv2.imread('../data/0006R0_f00930.png')
-    # img = cv2.resize(img, (1024, 720), interpolation=cv2.INTER_CUBIC)
-    print('img.shape:', img.shape)
+
+def randomFlipCv(img):
+    """ Randomly flip image """
+    flip = random.random() < 0.5
+    if flip:
+        img = cv2.flip(img, 1)
+    return img
+
+
+def hsvShiftPil(img):
+    hue = 0.1
+    sat = 1.5
+    val = 1.5
+
+    dh = random.uniform(-hue, hue)
+    ds = random.uniform(1, sat)
+    if random.random() < 0.5:
+        ds = 1 / ds
+    dv = random.uniform(1, val)
+    if random.random() < 0.5:
+        dv = 1 / dv
+
+    img = img.convert('HSV')
+    channels = list(img.split())
+
+    def change_hue(x):
+        x += int(dh * 255)
+        if x > 255:
+            x -= 255
+        elif x < 0:
+            x += 0
+        return x
+
+    channels[0] = channels[0].point(change_hue)
+    channels[1] = channels[1].point(lambda i: min(255, max(0, int(i*ds))))
+    channels[2] = channels[2].point(lambda i: min(255, max(0, int(i*dv))))
+
+    img = Image.merge(img.mode, tuple(channels))
+    img = img.convert('RGB')
+    return img
+
+
+def hsvShiftCv(img):
+    hue = 0.1
+    sat = 1.5
+    val = 1.5
+
+    dh = random.uniform(-hue, hue)
+    ds = random.uniform(1, sat)
+    if random.random() < 0.5:
+        ds = 1 / ds
+    dv = random.uniform(1, val)
+    if random.random() < 0.5:
+        dv = 1 / dv
+
+    img = img.astype(np.float32) / 255.0
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+
+    def wrap_hue(x):
+        x[x >= 360.0] -= 360.0
+        x[x < 0.0] += 360.0
+        return x
+
+    img[:, :, 0] = wrap_hue(img[:, :, 0] + (360.0 * dh))
+    img[:, :, 1] = np.clip(ds * img[:, :, 1], 0.0, 1.0)
+    img[:, :, 2] = np.clip(dv * img[:, :, 2], 0.0, 1.0)
+
+    img = cv2.cvtColor(img, cv2.COLOR_HSV2RGB)
+    img = (img * 255).astype(np.uint8)
+    return img
+
+
+if __name__ == '__main__':
+    test_num = 100
+    img = Image.open('../data/0006R0_f00930.png')
+    img = img.resize((1024, 720))
+    out_img = img
     start_time = time.time()
     for _ in range(test_num):
-        out_img = randomCropLetterboxCv(img)
-        cv2.imshow('img', out_img)
-        cv2.waitKey(0)
+        out_img = randomCropLetterboxPil(out_img)
+        out_img = randomFlipPil(out_img)
+        out_img = hsvShiftPil(out_img)
+        # plt.imshow(out_img)
+        # plt.show()
+    end_time = time.time()
+    print('cost time:', (end_time-start_time)/test_num)
+
+    test_num = 100
+    img = cv2.imread('../data/0006R0_f00930.png')
+    img = cv2.resize(img, (1024, 720), interpolation=cv2.INTER_CUBIC)
+    out_img = img
+    # print('img.shape:', img.shape)
+    start_time = time.time()
+    for _ in range(test_num):
+        out_img = randomCropLetterboxCv(out_img)
+        out_img = randomFlipCv(out_img)
+        out_img = hsvShiftCv(out_img)
+        # cv2.imshow('img', out_img)
+        # cv2.waitKey(0)
     end_time = time.time()
     print('cost time:', (end_time-start_time)/test_num)
